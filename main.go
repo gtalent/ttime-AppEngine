141d40549729d0d56fcce9b0c367df151da192f1
Gary Talent <gtalent2@gmail.com>
2011-06-11 00:05:03 -0500

    Done.

--- main.go
+++ main.go
@@ -13,19 +13,28 @@ func gdate() string {
 	ny.Month = 1
 	ny.Day = 1
 	ny.Zone = "CST"
-	return strconv.Itoa64((now.Seconds() - ny.Seconds()) / 86400) + ", " + strconv.Itoa64(now.Year)
+	return strconv.Itoa64((now.Seconds()-ny.Seconds())/86400) + ", " + strconv.Itoa64(now.Year)
 }
 
 func gtime() string {
 	now := time.LocalTime()
-	hour := int((float64(now.Hour) / 24) * 10)
-	minute := int((float64(now.Minute) / 60) * 100)
-	second := int((float64(now.Second) / 60) * 1000)
-	return strconv.Itoa(hour) + ":" + strconv.Itoa(minute) + ":" + strconv.Itoa(second)
+	var tsec int64
+	{
+		var t time.Time = *now
+		t.Hour = 0
+		t.Minute = 0
+		t.Second = 0
+		tsec = now.Seconds() - t.Seconds()
+	}
+	hour := int((float64(tsec) / 86400) * 10)
+	tsec -= int64(8640 * hour)
+	minute := int((float64(tsec) / 8640) * 100)
+	tsec -= int64((864 / 100) * minute)
+	second := int((float64(tsec) / 864) * 1000)
+	return strconv.Itoa(int(hour)) + ":" + strconv.Itoa(int(minute)) + ":" + strconv.Itoa(int(second))
 }
 
 func main() {
 	fmt.Println("Date:", gdate())
 	fmt.Println("Time:", gtime())
 }
-
